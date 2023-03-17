e796ac42ffad10e26e8fd35197e7c39233aaffe6
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-03-17 13:40:20 +0100

    Made the game-loop into method in prep for start-menue

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Dynamic;
+﻿using System;
+using System.Dynamic;
+using System.Numerics;
 using System.Security.Principal;
+using System.Threading;
 using static System.Console;
-namespace Assignment_2._1_RPG
+namespace _2._1
 {
     internal class Program
     {
@@ -10,35 +13,8 @@ namespace Assignment_2._1_RPG
         public static int foundMonsters = 0;
         public static int timeSpent = 0;
         public static int restTimes = 0;
-        public static Random random= new Random();
-        public static int DieSide(int sides)
-        {
-            int number = random.Next(1, sides + 1);
-            return number;
-        }
-        public static int DiceSide(int number, int sides)
-        {
-            int sum = 0;
-            for (int i = 0; i < number; i++)
-                sum += DieSide(sides);
-            return sum;
-        }
-        public class Character
-        {
-            public string Name { get; private set; }
-            public int hitPoints;
-            public Character(string name, int hp) { Name = name; hitPoints = hp; ArmourType = ""; WeaponType = ""; }
-            public int ParryDice { get; private set; }
-            public int ParrySides { get; private set; }
-            public string WeaponType { get; private set; }
-            public int WeaponDice { get; private set; }
-            public int WeaponSides { get; private set; }
-            public string ArmourType { get; private set; }
-            public int Armour { get; private set; }
-            public void SetParry(int amount, int sides) { ParryDice = amount; ParrySides = sides; }
-            public void SetWeapon(string type, int amount, int sides) { WeaponType = type; WeaponDice = amount; WeaponSides = sides; }
-            public void SetArmour(string type,int strength) { ArmourType = type; Armour = strength; }
-        }
+        public static Random random = new Random();
+
         public static void Fight(Character p1, Character p2)
         {
             while (p1.hitPoints > 0 && p2.hitPoints > 0)
@@ -59,7 +35,7 @@ namespace Assignment_2._1_RPG
                     break;
                 }
             }
-        } 
+        }
         public static int Attack(Character attacker, Character defender)
         {
             int HP = WeaponRoll(attacker);
@@ -76,31 +52,41 @@ namespace Assignment_2._1_RPG
                 return 0;
             }
         }
-        public static int WeaponRoll(Character p) { return DiceSide(p.WeaponDice, p.WeaponSides); }
-        public static int ParryRoll(Character p) { return DiceSide(p.ParryDice, p.ParrySides); }   
+        public static int WeaponRoll(Character p) { return p.weapon.Roll(); }
+        public static int ParryRoll(Character p) { return p.parry.Roll(); }
         static void Main(string[] args)
         {
             WriteLine("      ~~~~~~Välkommen äventyrare!~~~~~~\nDitt namn: ");
-            string name=ReadLine();
-            Character Player = new(name, 25); Player.SetParry(1, 4); Player.SetWeapon("kortsvärd",1,10); Player.SetArmour("läder", 2);
-            Character Monster = new("Monster",35); Monster.SetParry(0, 0); Monster.SetWeapon("klor",2, 6); Monster.SetArmour("tjock hud", 2 );
-            WriteLine(Player.Name + " gör entré. Du har 60 min på dig.");            
-            while (Player.hitPoints>0&&timeSpent<60)
+            string name = ReadLine();
+            Character Player = new(name, 25); Player.SetParry(1, 4); Player.SetWeapon("kortsvärd", 1, 10); Player.SetArmour("läder", 2);
+            //TODO  start menu, save, load etc
+            Enter(Player);
+
+            if (Player.hitPoints < 1)
+                WriteLine($"\n    xxx {Player.Name} dog xxx \nBättre lycka nästa gång!");
+            else if (timeSpent >= 60)
+                WriteLine("\nTiden är ute!");
+            WriteLine($"Spelet är över. {Player.Name} tog sig igenom {clearedRooms} rum och dödade {defeatedMonsters} av {foundMonsters} monster.\n{Player.Name} spenderade {timeSpent} minuter i äventyret, och vilade {restTimes} gånger.");
+        }
+        static void Enter(Character Player)
+        {
+            WriteLine(Player.Name + " gör entré. Du har 60 min på dig.");
+            while (Player.hitPoints > 0 && timeSpent < 60)
             {
                 int nextRoom = random.Next(0, 2);
                 if (nextRoom == 0)
-                { 
+                {
                     timeSpent += 1;
                     clearedRooms++;
                     WriteLine("\n... Rummet är tomt ... ");
                     goto ClearedRoomChoice;
                 }
-                else if ( nextRoom== 1)
-                { 
+                else if (nextRoom == 1)
+                {
                     foundMonsters++;
-                    Monster = new("Monster", random.Next(20, 41)); Monster.SetParry(0, 0); Monster.SetWeapon("Klor", 2, random.Next(4, 7)); Monster.SetArmour("tjock hud", random.Next(0, 3));
+                    Character Monster = new("Monster", random.Next(20, 41)); Monster.SetParry(0, 0); Monster.SetWeapon("Klor", 2, random.Next(4, 7)); Monster.SetArmour("tjock hud", random.Next(0, 3));
                     WriteLine("\nDu möter ett monster!");
-                    WriteLine($"{Monster.Name} har {Monster.hitPoints} hp. ATK {Monster.WeaponType}: {Monster.WeaponDice} x D{Monster.WeaponSides}. DEF parry: {Monster.ParryDice} x D{Monster.ParrySides}. DEF {Monster.ArmourType}: {Monster.Armour}. ");
+                    WriteLine($"{Monster.Name} har {Monster.hitPoints} hp. ATK {Monster.WeaponType}: {Monster.weapon.amount} x D{Monster.weapon.sides}. DEF parry: {Monster.parry.amount} x D{Monster.parry.sides}. DEF {Monster.ArmourType}: {Monster.Armour}. ");
                     Write("Fight? (Y/N) ");
                     string fightChoice = ReadLine();
                     if (fightChoice == "N" || fightChoice == "n")
@@ -121,35 +107,31 @@ namespace Assignment_2._1_RPG
                 }
             ClearedRoomChoice:
                 Write("Gå vidare? \nY/N: ");
-                string clearedRoomChoice = ReadLine();
-                if (clearedRoomChoice == "Y")
+                string clearedRoomChoice = ReadLine().ToLower();
+                if (clearedRoomChoice == "y")
                     continue;
-                else if (clearedRoomChoice == "N")
+                else if (clearedRoomChoice == "n")
                     break;
                 else
                     goto Rest;
-            Rest:
+                Rest:
                 Write($"\nDu har {Player.hitPoints} hp. Gå vidare? \nSkriv [r] för att vila 15 min och fylla på 15 HP, [c] för att fortsätta till nästa rum, [q] för att avsluta: ");
-                string restChoice = ReadLine();
-                if (restChoice == "q" || restChoice == "Q")
+                string restChoice = ReadLine().ToLower();
+                if (restChoice == "q")
                     break;
-                else if (restChoice == "r" || restChoice == "R")
-                { 
+                else if (restChoice == "r")
+                {
                     Player.hitPoints += 15;
                     timeSpent += 15;
                     restTimes++;
-                    goto Rest; 
+                    goto Rest;
                 }
-                else if (restChoice == "c" || restChoice == "C")
+                else if (restChoice == "c")
                     continue;
                 else
                     goto Rest;
             }
-            if(Player.hitPoints < 1)
-                WriteLine($"\n    xxx {Player.Name} dog xxx \nBättre lycka nästa gång!"); 
-            else if (timeSpent >= 60)
-                WriteLine("\nTiden är ute!");
-            WriteLine($"Spelet är över. {Player.Name} tog sig igenom {clearedRooms} rum och dödade {defeatedMonsters} av {foundMonsters} monster.\n{Player.Name} spenderade {timeSpent} av 60 minuter i äventyret, och vilade {restTimes} gånger.");
+            return;
         }
     }
 }        
