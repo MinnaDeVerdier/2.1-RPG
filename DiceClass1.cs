8e3e7c1578a4a365a96e53769e0a87febf7b5db1
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-03-17 13:09:32 +0100

    Made a separate class for creating dice objects

---
+++ DiceClass1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    class Dice
+    {
+        public int amount;
+        public int sides;
+        public Dice(int amount, int sides) 
+        {
+            this.amount = amount;
+            this.sides = sides;
+        }
+        public int Roll()
+        {
+            Random random = new Random();
+            int result = 0;
+            for (int i = 0; i < amount; i++)
+                result += random.Next(1, sides + 1);
+            return result;
+        }
+    }
+}
+  
+
